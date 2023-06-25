e39afb640da3e907e2c76670b52ac3afe1858ff3
R1013-T <nosuke1013@icloud.com>
2023-06-25 12:56:14 +0900

    setup test

--- Gemfile
+++ Gemfile
@@ -40,6 +40,11 @@ group :development do
   gem 'spring'
 end
 
+group :test do
+  # テスト結果を色付け
+  gem 'minitest-reporters', '~> 1.6'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
+# gem 'minitest-reporters'を追加したので、以下を追記
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+
+  parallelize_setup do |worker|
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
